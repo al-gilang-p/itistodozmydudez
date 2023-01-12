93841bd039651e01efb26d9c835e724bce7c8c52
Alwin Gilang Permana <al.gilang.p@gmail.com>
2023-01-12 08:34:28 +0700

    refactor: add src folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +21,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={styles.main}>
-                <div className="font-bold">Lets get started</div>
+                <div className="font-bold">Is it still working?</div>
             </main>
         </>
     )
